30fe68c3f4234af3c0bf7f5896f4174c9cc10363
joao vitor <githubconta2021@gmail.com>
2023-12-14 11:19:40 -0300

    manipulaçao de arquivo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace monipula_arquivo {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = @"c:\Downloads\arquivo.txt"; 
+            string targePath = @"c:\Downloads\arquivo2.txt";
+             
+             try
+             {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targePath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+             }
+             catch (IOException e)
+             {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+             }
+ 
+        }
+    }
+}
